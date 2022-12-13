332e05535fa3a17e72c10b8ee2d49de35e64b4dd
Isingizwe12 <118433854+Isingizwe12@users.noreply.github.com>
2022-12-13 22:27:11 +0200

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,21 +23,48 @@ function linkAction() {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll('section[id]')
+// const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+// // function scrollActive(){
+// //     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+// //     sections.forEach(current =>{
+// //         const sectionHeight = current.offsetHeight
+// //         const sectionTop = current.offsetTop - 50;
+// //         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
+// //         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+// //             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+// //         }else{
+// //             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+// //         }
+// //     })
+// // }
+// window.addEventListener('scroll', scrollActive)
+
+
+/*===== Focus ======*/
+const inputs = document.querySelectorAll(".form__input");
+
+/*==== Add focus === */
+
+function addFocus() {
+  let parent = this.parentNode.parentNode;
+  parent.classList.add("focus");
 }
-window.addEventListener('scroll', scrollActive)
+
+/*==== Remove focus === */
+
+function removeFocus() {
+  let parent = this.parentNode.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+/**
+ * Adding event to fire off the focus on inputs
+ */
+inputs.forEach((input) => {
+  input.addEventListener("focus", addFocus);
+  input.addEventListener("blur", removeFocus);
+});
